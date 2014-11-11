b53d8b30c9ceb8090f80421e24655c1b0479319c
s10n <a@akaiv.com>
2014-11-11 18:30:30 +0900

    functions.php: 메타 정보 일부 수정

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -34,7 +34,7 @@ function akaiv_head() { ?>
   <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/images/favicon/apple-touch-icon-76x76.png">
   <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/images/favicon/apple-touch-icon-152x152.png">
   <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/images/favicon/apple-touch-icon-180x180.png">
-  <meta name="apple-mobile-web-app-title" content="wp-akaiv">
+  <meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?>">
   <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/images/favicon/favicon-192x192.png" sizes="192x192">
   <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/images/favicon/favicon-160x160.png" sizes="160x160">
   <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/images/favicon/favicon-96x96.png" sizes="96x96">
@@ -43,14 +43,13 @@ function akaiv_head() { ?>
   <meta name="msapplication-TileColor" content="#777777">
   <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/images/favicon/mstile-144x144.png">
   <meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/images/favicon/browserconfig.xml">
-  <meta name="application-name" content="wp-akaiv">
+  <meta name="application-name" content="<?php bloginfo( 'name' ); ?>">
 
   <?php if ( is_single() ) : ?>
   <meta property="og:title" content="<?php single_post_title(''); ?>">
   <meta property="og:description" content="<?php $excerpt = get_the_excerpt(); if ( $excerpt != '' ) echo $excerpt; ?>">
   <meta property="og:url" content="<?php the_permalink(); ?>">
   <meta property="og:type" content="article">
-  <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/fb-image.jpg">
 
   <?php else : ?>
   <meta property="og:site_name" content="<?php bloginfo('name'); ?>">
