c0b250dce19502c0bf3f62ecc936fe9e67674eab
s10n <a@akaiv.com>
2014-11-13 20:52:14 +0900

    피드: the_excerpt_rss에 필터를 적용하는 방식으로 변경

--- inc/article.php
+++ inc/article.php
@@ -2,14 +2,13 @@
 /* 요약에서 auto p 제거 */
 remove_filter( 'the_excerpt', 'wpautop' );
 
-/* 본문에 URL 명시(feed) */
-function akaiv_insert_content_footnote($content) {
-  if( is_feed() ) :
-    if ( has_post_thumbnail() ) :
-      $content.= '<p class="post-thumbnail">'.get_the_post_thumbnail(null, 'large').'</p>';
-    endif;
-    $content.= '<p>원문 바로가기: <a href="'.akaiv_get_url().'">'.akaiv_get_url().'</a></p>';
+/* 피드 */
+function akaiv_insert_footnote($content) {
+  $content = '<p>'.$content.'</p>';
+  if ( has_post_thumbnail() ) :
+    $content = '<p>'.get_the_post_thumbnail(null, 'large').'</p>'.$content;
   endif;
+  $content.= '<p>원문 바로가기: <a href="'.akaiv_get_url().'">'.akaiv_get_url().'</a></p>';
   return $content;
 }
-add_filter( 'the_content', 'akaiv_insert_content_footnote' );
+add_filter( 'the_excerpt_rss', 'akaiv_insert_footnote' );
