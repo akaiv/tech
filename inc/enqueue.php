6bbfcab913b433a338f4611e3bc2615b64a3a442
s10n <a@akaiv.com>
2014-11-26 10:22:02 +0900

    [스타일] 구글 웹폰트를 스타일시트가 아닌 워드프레스 템플릿태그로 임포트

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -16,6 +16,7 @@ function akaiv_scripts() {
     );
   endif;
   wp_enqueue_style(  'project-style',    get_template_directory_uri() . $assets['css']);
+  wp_enqueue_style(  'lato',             add_query_arg( 'family', urlencode( 'Lato:400,700' ), '//fonts.googleapis.com/css' ), array(), null );
   wp_enqueue_script( 'modernizr',        get_template_directory_uri() . $assets['modernizr'], array(), null, true);
   // if( is_archive() ) { wp_enqueue_script( 'jquery-masonry' ); }
   wp_enqueue_script( 'project-script',   get_template_directory_uri() . $assets['js'], array( 'jquery' ), null, true );
