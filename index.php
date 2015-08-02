82b860cc2212be2a2f591fe981c4b9bf31ac538b
s10n <a@akaiv.com>
2015-08-03 02:02:28 +0900

    [리팩토링] 기존의 2중 for문 형태를 제거하고 일반적인 WP_Query 형태로 변경, 모양은 같음

--- index.php
+++ index.php
@@ -3,33 +3,12 @@ get_header();
 akaiv_before_content(); ?>
 
 <?php
-/* 카테고리로 정렬하기 위해 Array 준비 */
-$posts_by_category = array();
-$category_ids = get_terms( 'category', array(
-  'fields'  => 'ids',
-  'orderby' => 'name',
-  'order'   => 'DESC'
-) );
-foreach( $category_ids as $id )
-  $posts_by_category[$id] = array();
-foreach( $posts as $post ) :
-  $categories = get_the_terms( $post->ID, 'category' );
-  foreach( $categories as $key => $value ) :
-    $category_id = $key;
-    break;
-  endforeach;
-  $posts_by_category[(string)$category_id][] = $post;
-endforeach; ?>
-
-<?php
 if ( have_posts() ) :
 
   akaiv_page_header();
-  foreach( $posts_by_category as $category_id => $posts ) :
-    foreach( $posts as $post ) :
-      include 'templates/article.php';
-    endforeach;
-  endforeach; /* 카테고리 */
+  while ( have_posts() ) : the_post();
+    get_template_part( 'templates/article' );
+  endwhile;
   akaiv_paginate_links();
 
 else :
--- templates/article.php
+++ templates/article.php
@@ -1,32 +1,22 @@
 <?php
-  $title       = $post->post_title;
-  $url         = get_post_meta( $post->ID, 'wpcf-url', true );
-  $excerpt     = get_the_excerpt();
-
-  if ( is_category() ) :
-    $tag       = get_tag( $tag_id );
-    $tag_name  = $tag->name;
-    $tag_link  = get_tag_link( $tag_id );
-    $term_name = $tag_name;
-    $term_link = $tag_link;
-
-  else :
-    $cat       = get_category( $category_id );
-    $cat_name  = $cat->cat_name;
-    $cat_link  = get_category_link( $category_id );
-    $term_name = $cat_name;
-    $term_link = $cat_link;
-
-  endif;
+  $categories_list = get_the_category_list( ', ' );
+  $tags_list       = get_the_tag_list( '', ', ', '' );
+  $excerpt         = get_the_excerpt();
 ?>
 
 <article class="post hentry<?php if ( $excerpt != '' ) echo ' has-excerpt'; ?>">
   <div class="term-link-wrapper">
-    <a class="term-link" href="<?php echo $term_link; ?>"><?php echo $term_name; ?></a>
+    <?php
+      if ( is_category() ) :
+        echo $tags_list;
+      else :
+        echo $categories_list;
+      endif;
+    ?>
   </div>
   <div class="entry-wrapper">
     <h1 class="entry-title">
-      <a href="<?php echo $url; ?>" target="_blank" rel="bookmark"><?php echo $title; ?></a>
+      <a href="<?php akaiv_the_url(); ?>" target="_blank" rel="bookmark"><?php akaiv_the_title(); ?></a>
     </h1>
     <?php if ( $excerpt != '' ) echo '<p class="entry-summary">'.$excerpt.'</p>'; ?>
     <div class="entry-meta"><?php
